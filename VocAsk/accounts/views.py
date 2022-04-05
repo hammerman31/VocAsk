907de10b94d25c874cf4e6c930d98eeee49afe6b
hammerman31 <vonalfingen@gmail.com>
2022-04-05 19:17:15 +0200

    Loogin und Logout

--- VocAsk/accounts/views.py
+++ VocAsk/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
@@ -7,10 +8,27 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             #log user in
+            login(request, user)
             return redirect('index')  
     else:
         form = UserCreationForm()
-    
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log user in
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('index')

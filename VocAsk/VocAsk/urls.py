1174fa91751b163d5e6cc186f86e2d6eef31cbbb
hammerman31 <vonalfingen@gmail.com>
2022-04-05 16:09:05 +0200

    Grundstruktur der Accountstruktur

--- VocAsk/VocAsk/urls.py
+++ VocAsk/VocAsk/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from vaApp import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('about/', views.about),
+    path('anleitung/', views.anleitung),
+    path('accounts/', include('accounts.urls')),
+
 ]
---
+++ VocAsk/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log user in
+            return redirect('index')  
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'accounts/signup.html', {'form': form})

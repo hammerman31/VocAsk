b61d73f0570bb5d4d020e5eef5b61a63a6c20a25
hammerman31 <vonalfingen@gmail.com>
2022-07-05 16:08:53 +0200

    gespeicherte Vokabeln Auswählen: Ausnahme Hinzugefügt

--- VocAsk/VocAsk/settings.py
+++ VocAsk/VocAsk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=ki9dvw&=+a9bl7d_l%w+ozuyrg!4#z03d!40h)v$qwp_)&qf(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

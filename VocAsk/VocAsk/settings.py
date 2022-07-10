0b4f98b8a2201c2256266d2cc1556ac0f803ecd6
hammerman31 <vonalfingen@gmail.com>
2022-07-10 11:45:35 +0200

    Update für static root und allowed hosts

--- VocAsk/VocAsk/settings.py
+++ VocAsk/VocAsk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=ki9dvw&=+a9bl7d_l%w+ozuyrg!4#z03d!40h)v$qwp_)&qf(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hammerman31.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'vaApp/static']
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'VocAsk/vaApp/static/'
+STATIC_ROOT = 'home/hammerman31/VocAsk/VocAsk/vaApp/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

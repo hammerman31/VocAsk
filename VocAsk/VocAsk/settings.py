f8c878fea7d3a6ebed5bdf44dd1059305ae0997e
hammerman31 <vonalfingen@gmail.com>
2022-07-10 11:59:28 +0200

    upadate static root

--- VocAsk/VocAsk/settings.py
+++ VocAsk/VocAsk/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'vaApp/static']
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'home/hammerman31/VocAsk/VocAsk/vaApp/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = 'home/hammerman31/VocAsk/VocAsk/vaApp/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

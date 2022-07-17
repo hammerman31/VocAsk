bb3ea832f5f640fba2af8363e8d058ee0b800e9f
hammerman31 <vonalfingen@gmail.com>
2022-07-17 16:04:34 +0200

    Anpassungen für Mobil Version

--- VocAsk/VocAsk/settings.py
+++ VocAsk/VocAsk/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [BASE_DIR / 'vaApp/static']
+STATICFILES_DIRS = [BASE_DIR / 'vaApp/static']
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = 'home/hammerman31/VocAsk/VocAsk/vaApp/static/'
 # Default primary key field type

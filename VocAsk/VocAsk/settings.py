9ca7786ec7deb8d646beadf8e034ee368575b346
hammerman31 <vonalfingen@gmail.com>
2022-09-08 16:47:57 +0200

    Fehlerbehebung

--- VocAsk/VocAsk/settings.py
+++ VocAsk/VocAsk/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/main/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "main/static"]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = "home/hammerman31/VocAsk/VocAsk/main/static/"
